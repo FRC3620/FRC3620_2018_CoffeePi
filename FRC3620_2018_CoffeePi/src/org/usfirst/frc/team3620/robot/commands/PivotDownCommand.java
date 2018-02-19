4c65ccfcdd35c90fbcc6aed9b35dfdb0e98f62d0
Dashiel <533237@stjoebears.com>
2018-02-19 17:12:56 -0500

    Changed the pivot to %output and added PID stuff into the intakesubsystem

--- FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/commands/PivotDownCommand.java
+++ FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/commands/PivotDownCommand.java
@@ -21,7 +21,7 @@ public class PivotDownCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.intakeSubsystem.pivotDown(0.4);
+    	Robot.intakeSubsystem.pivotDown(0.08);
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/subsystems/IntakeSubsystem.java
+++ FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/subsystems/IntakeSubsystem.java
@@ -8,6 +8,8 @@ import org.usfirst.frc.team3620.robot.commands.IntakeCubeCommand;
 import org.usfirst.frc3620.logger.EventLogging;
 import org.usfirst.frc3620.logger.EventLogging.Level;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -31,6 +33,68 @@ public class IntakeSubsystem extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+	public static final int kSpeedPIDLoopIdx = 0;
+	public static final int kTimeoutMs = 0;
+	public static final boolean kMotorInvert = false;
+	public static double kPSpeed = 0;
+	public static double kISpeed = 0;
+	public static double kDSpeed = 0;
+	public static double kFSpeed = 0;
+	public static double kIZoneSpeed = 0;
+	public static int peakSpeedHigh = 0;
+	public static int positionErrorMargin = 50;
+	public static int motionMagicCruiseVel;
+	public static int motionMagicAccel;
+
+	public IntakeSubsystem() {
+		super();
+
+		intakePivot.config_kF(kSpeedPIDLoopIdx, kFSpeed, kTimeoutMs);
+		intakePivot.config_kP(kSpeedPIDLoopIdx, kPSpeed, kTimeoutMs);
+		intakePivot.config_kI(kSpeedPIDLoopIdx, kISpeed, kTimeoutMs);
+		intakePivot.config_kD(kSpeedPIDLoopIdx, kDSpeed, kTimeoutMs);
+		
+
+		// Setting feedback device type
+		intakePivot.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+		intakePivot.setSensorPhase(true);
+
+	}
+	
+	public double readEncoder() {
+
+		int encoderPos = intakePivot.getSensorCollection().getQuadraturePosition();
+		return encoderPos;
+
+	}
+	
+	public void resetEncoder() {
+
+		int sensorPos = 0;
+		intakePivot.setSelectedSensorPosition(sensorPos, kSpeedPIDLoopIdx, 10);
+	}
+
+	public void setPIDParameters(double P, double I, double D, double F) {
+		
+		intakePivot.config_kP(0, P, 10);
+		intakePivot.config_kI(0, I, 10);
+		intakePivot.config_kD(0, D, 10);
+		intakePivot.config_kF(0, F, 10);
+		
+		
+		
+	}
+
+	public void configMotionMagic(int acceleration, int velocity) {
+		
+		motionMagicAccel = acceleration;
+		motionMagicCruiseVel = velocity;
+		
+		intakePivot.configMotionCruiseVelocity(kSpeedPIDLoopIdx, motionMagicCruiseVel);
+		intakePivot.configMotionAcceleration(kSpeedPIDLoopIdx, motionMagicAccel);
+		
+	}
+
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
@@ -49,7 +113,7 @@ public class IntakeSubsystem extends Subsystem {
    
    public void pivotUp(double speed){
 	   if(intakePivot != null) {
-		   intakePivot.set(speed);
+		   intakePivot.set(ControlMode.PercentOutput, speed);
 	   } else {
 		  logger.info("Tried to pivot up - no CANTalons!");
 	   }
@@ -57,7 +121,7 @@ public class IntakeSubsystem extends Subsystem {
    
    public void pivotDown(double speed){
 	   if(intakePivot != null) {
-		   intakePivot.set(-speed);
+		   intakePivot.set(ControlMode.PercentOutput, -speed);
 	   } else {
 		  logger.info("Tried to pivot down - no CANTalons!");
 	   }
