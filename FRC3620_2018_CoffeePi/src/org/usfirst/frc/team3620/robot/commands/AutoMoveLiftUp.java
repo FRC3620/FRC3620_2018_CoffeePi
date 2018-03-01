2264312e0f220c9aeb008da3731ab65e866e889d
chaynes2019 <chaynes2019@gmail.com>
2018-02-28 19:05:59 -0500

    Sending it on down to Horacio for Pivot Tuning.

--- FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/commands/AutoMoveLiftUp.java
+++ FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/commands/AutoMoveLiftUp.java
@@ -12,8 +12,11 @@ import org.slf4j.Logger;
 public class AutoMoveLiftUp extends Command {
 	Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
 	double encoderPos;
+	int startingEncoderPos;
 	double requestedEncoderPos;
-	double slowDownPoint = requestedEncoderPos - 768;
+	int oneFootInTics;
+	double slowDownPoint = requestedEncoderPos - oneFootInTics;
+	int speedUpPoint;
 	double desiredStartingPower;
 	double desiredEndingPower;
     public AutoMoveLiftUp() {
@@ -26,39 +29,18 @@ public class AutoMoveLiftUp extends Command {
     //1440 ticks of encoder = 16.875 inches
     protected void initialize() {
     	logger.info("Starting AutoMoveLiftUp Command");
-    	requestedEncoderPos = 4949;
-    	desiredStartingPower = 0.125;
-    	desiredEndingPower = 0.1625;
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	encoderPos = Robot.liftSubsystem.readEncoder();
-    	if(encoderPos <= 512) {
-    		Robot.liftSubsystem.moveElevatorUp(0.8*(1/Math.cosh(((encoderPos - 512)/-233.38))));
-    		System.out.println("We're accelerating to Speed");
-    	} else if(encoderPos > 512 && encoderPos < slowDownPoint) {
-    		Robot.liftSubsystem.moveElevatorUp(0.8);
-    		System.out.println("At Cruise Altitude and Speed");
-    	} else if(encoderPos >= slowDownPoint && encoderPos < requestedEncoderPos) {
-    		Robot.liftSubsystem.moveElevatorUp(0.8*(1/Math.cosh(((encoderPos - requestedEncoderPos)/281.32))));
-    		System.out.println("Nearing Destination");
-    	} else if(encoderPos >= requestedEncoderPos) {
-    		
-    		System.out.println("We overshot.");
-    	}
+    	
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	if(Robot.liftSubsystem.readEncoder() > requestedEncoderPos || Robot.liftSubsystem.isTopLimitDepressed()) {
-    		Robot.liftSubsystem.brace();
-    		System.out.println("Exiting Moving UP");
-    		return true;
-    	}
-    	else{
-    		return false;
-    	}
+    	return false;
     }
 
     // Called once after isFinished returns true
