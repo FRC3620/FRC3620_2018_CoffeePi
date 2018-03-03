c1f689b13181f77a12375d49c1cbea34f48934e7
Kai3620 <kaiborah@yahoo.com>
2018-03-03 10:10:09 -0500

    Add subpath example

---
+++ FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/paths/Subpath_LeftScaleSide_AlleyCubeB.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team3620.robot.paths;
+
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Waypoint;
+
+//TODO Finish me!
+//TODO Test me!
+
+public class Subpath_LeftScaleSide_AlleyCubeB extends AbstractPath {
+
+	//Status: Not tested, unfinished
+	@Override
+	Waypoint[] getMyWaypoints() {
+		return new Waypoint[] {
+				
+		};
+	}
+}
