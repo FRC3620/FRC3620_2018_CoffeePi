d0f8821b520160d497f7f55f7c8e268d00f547e9
chaynes2019 <chaynes2019@gmail.com>
2018-03-23 13:02:47 -0400

    Lift Brake
    Applied 10-inch cushion when coming down with the lift so that brake is
    not implemented too soon.

--- FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/commands/ManualLiftOperatorCommand.java
+++ FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/commands/ManualLiftOperatorCommand.java
@@ -49,8 +49,11 @@ public class ManualLiftOperatorCommand extends Command {
     		}
     		else if(joyPos > 0.2 && Robot.liftSubsystem.isBottomLimitDepressed() == false) {
     			if (true) {
-    				if( Robot.liftSubsystem.readEncoderInInches() < 20){
-    					joyPos = joyPos * 0.23;
+    				if(Robot.liftSubsystem.readEncoderInInches() < 20) {
+    					joyPos = joyPos*0.23;
+    				}
+    				else if(Robot.liftSubsystem.readEncoderInInches() < 30){
+    					joyPos = joyPos * (1 - (((30 - Robot.liftSubsystem.readEncoderInInches())/10)*0.77));
     				}
     				//		if( Robot.liftSubsystem.readEncoderInInches() < 8){
     				//		joyPos = joyPos * 0.2;
