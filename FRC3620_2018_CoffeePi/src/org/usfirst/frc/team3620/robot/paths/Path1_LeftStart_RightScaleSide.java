69c10646d9eae94725e362d3648dbc40183284d5
Dashiel <533237@stjoebears.com>
2018-04-21 11:14:20 -0400

    Tweaking of allycube by dashiel and horacio

--- FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/paths/Path1_LeftStart_RightScaleSide.java
+++ FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/paths/Path1_LeftStart_RightScaleSide.java
@@ -36,10 +36,10 @@ public class Path1_LeftStart_RightScaleSide extends AbstractPath {
 				new Waypoint(21.30, 9.75, Pathfinder.d2r(0)),  */
 				
 				new Waypoint(1.58, 22.91, Pathfinder.d2r(0)),
-				new Waypoint(11.425, 23.0, Pathfinder.d2r(0)),
+				new Waypoint(11.5, 23.0, Pathfinder.d2r(0)),  // was new Waypoint(11.425, 23.0, Pathfinder.d2r(0)),
 				//Mark: "18.45 is smack dab in the middle"
-				new Waypoint(17.45, 18.5, Pathfinder.d2r(-90)),
-				new Waypoint(17.95, 12.95, Pathfinder.d2r(-90)),
+				new Waypoint(17, 18.5, Pathfinder.d2r(-90)),
+				new Waypoint(17.8, 12.95, Pathfinder.d2r(-90)),
 				new Waypoint(19.30, 9.75, Pathfinder.d2r(0)),
 				
 		};
