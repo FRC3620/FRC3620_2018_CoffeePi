85d7de30cd17a637c6c45d048ea3ff1fa8dd2065
Horacio Moreno <yayin1111@gmail.com>
2018-04-23 19:02:37 -0400

    Tweaked the waypoints,bumped up the P to help with consistency and lowered velocity on that path to make it more accurate.

--- FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/paths/Path1_LeftStart_RightScaleSide.java
+++ FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/paths/Path1_LeftStart_RightScaleSide.java
@@ -44,22 +44,22 @@ public class Path1_LeftStart_RightScaleSide extends AbstractPath {
 				new Waypoint(21.0, 9.75, Pathfinder.d2r(0)), */
 				
 				new Waypoint(1.58, 22.91, Pathfinder.d2r(0)),
-				new Waypoint(15.0, 23.0, Pathfinder.d2r(0)),  // was new Waypoint(11.425, 23.0, Pathfinder.d2r(0)),
+				new Waypoint(10.5, 23.0, Pathfinder.d2r(0)),  // was new Waypoint(11.425, 23.0, Pathfinder.d2r(0)),
 				//Mark: "18.45 is smack dab in the middle"
 				new Waypoint(18.4, 18.5, Pathfinder.d2r(-90)),
-				new Waypoint(18.4, 12.95, Pathfinder.d2r(-90)),
-				new Waypoint(21.0, 9.75, Pathfinder.d2r(0)),
+				new Waypoint(18.4, 11.5, Pathfinder.d2r(-90)),
+				new Waypoint(22.5, 7.50, Pathfinder.d2r(0)),
 				
 		};
 	}
 	
 	@Override
 	double getPathfinderGenVelocityMultiplier() {
-		return 1.0;
+		return 0.85;
 	}
 	@Override
 	double getPathfinderP() {
-		return 0.027;
+		return 0.17;
 	}
 	
 	@Override
