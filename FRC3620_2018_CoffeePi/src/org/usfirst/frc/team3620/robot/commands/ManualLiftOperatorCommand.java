549391dcb45fd72889e9848b320c6dabba9eb6ca
Horacio <yayin1111@gmail.com>
2018-09-25 18:40:31 -0400

    Added an if statement in the execute method of the manual lift operator command to stop it from going up if the arm is down

--- FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/commands/ManualLiftOperatorCommand.java
+++ FRC3620_2018_CoffeePi/src/org/usfirst/frc/team3620/robot/commands/ManualLiftOperatorCommand.java
@@ -15,115 +15,117 @@ import org.usfirst.frc.team3620.robot.OI;
 public class ManualLiftOperatorCommand extends Command {
 	Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
 
-    public ManualLiftOperatorCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.liftSubsystem);
-    }
+	public ManualLiftOperatorCommand() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.liftSubsystem);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	logger.info("Starting Manual Lift Command, encoder inches = {}", Robot.liftSubsystem.readEncoderInInches());
-    	
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		logger.info("Starting Manual Lift Command, encoder inches = {}", Robot.liftSubsystem.readEncoderInInches());
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	double joyPos = Robot.m_oi.getLiftJoystick();
-    	boolean highGear = Robot.liftSubsystem.isInHighGear();
-    	double encoderPos = Robot.liftSubsystem.readEncoderInInches();
-    	//	logger.info("" + joyPos);
-    //	logger.info("High Gear? = {}", highGear);
-    	if(highGear == false){
-    		if(joyPos > 0.2 && Robot.liftSubsystem.isBottomLimitDepressed() == false){
-    			Robot.liftSubsystem.climb(joyPos);
-    			logger.info("Starting HighGear? = {}", highGear);
-    		}
-    		else if(joyPos < -0.2 && Robot.liftSubsystem.isTopLimitDepressed() == false){
-    			Robot.liftSubsystem.moveElevatorUp(-joyPos, highGear);
-    		}
-    	} else{
+	}
 
-    		if(joyPos <  -0.2 && Robot.liftSubsystem.isTopLimitDepressed() == false) {
-    			if(joyPos < -0.85) {
-    				joyPos = -1.0;
-    			}
-    			Robot.liftSubsystem.moveElevatorUp(-joyPos);
-    			 //  		System.out.println("Moving Lift Up at JoyPos = " + joyPos);
-    		}
-    		else if(joyPos > 0.2 && Robot.liftSubsystem.isBottomLimitDepressed() == false) {
-    			if (true) {
-    				double originalJoyPos = joyPos;
-    				double h = 40;
-    				double l = 7;
-    				
-    			
-    				if(encoderPos < l) {
-    					joyPos = joyPos*0.18;
-    				
-    				}
-    				else if(encoderPos < h){
-    					joyPos = joyPos * (1 - (((h - encoderPos)/(h-l))*0.82));
-    				} else if(encoderPos > h) {
-    					joyPos = 0.8*joyPos;
-    				}
-    				if( joyPos > 0.85) {
-    					joyPos = 1.0;
-    				}
-    				
-    		//		logger.info("encoder Pos = {}, original JoystickPos = {}, final JoystickPos = {}", encoderPos, originalJoyPos, joyPos);
-    				Robot.liftSubsystem.moveElevatorDown(joyPos);
-    				//   		System.out.println("Moving Lift Down");
-    			} else {
-    				Robot.liftSubsystem.moveAtManualSpeedGiven(0);
-    			}
-    		}
-    	}
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if(Robot.intakeSubsystem.homeButtonIsPressed()) {
+			double joyPos = Robot.m_oi.getLiftJoystick();
+			boolean highGear = Robot.liftSubsystem.isInHighGear();
+			double encoderPos = Robot.liftSubsystem.readEncoderInInches();
+			//	logger.info("" + joyPos);
+			//	logger.info("High Gear? = {}", highGear);
+			if(highGear == false){
+				if(joyPos > 0.2 && Robot.liftSubsystem.isBottomLimitDepressed() == false){
+					Robot.liftSubsystem.climb(joyPos);
+					logger.info("Starting HighGear? = {}", highGear);
+				}
+				else if(joyPos < -0.2 && Robot.liftSubsystem.isTopLimitDepressed() == false){
+					Robot.liftSubsystem.moveElevatorUp(-joyPos, highGear);
+				}
+			} else{
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-    	double joyPos = Robot.m_oi.getLiftJoystick();
-    	if(Robot.liftSubsystem.isBottomLimitDepressed()) {
-    		System.out.println("Bottom Switch just got pushed.");
-    		Robot.liftSubsystem.resetEncoder();
-    		
-    		return true;
-    	}
-    /*	else if(Robot.liftSubsystem.isTopLimitDepressed()) {
+				if(joyPos <  -0.2 && Robot.liftSubsystem.isTopLimitDepressed() == false) {
+					if(joyPos < -0.85) {
+						joyPos = -1.0;
+					}
+					Robot.liftSubsystem.moveElevatorUp(-joyPos);
+					//  		System.out.println("Moving Lift Up at JoyPos = " + joyPos);
+				}
+				else if(joyPos > 0.2 && Robot.liftSubsystem.isBottomLimitDepressed() == false) {
+					if (true) {
+						double originalJoyPos = joyPos;
+						double h = 40;
+						double l = 7;
+
+
+						if(encoderPos < l) {
+							joyPos = joyPos*0.18;
+
+						}
+						else if(encoderPos < h){
+							joyPos = joyPos * (1 - (((h - encoderPos)/(h-l))*0.82));
+						} else if(encoderPos > h) {
+							joyPos = 0.8*joyPos;
+						}
+						if( joyPos > 0.85) {
+							joyPos = 1.0;
+						}
+
+						//		logger.info("encoder Pos = {}, original JoystickPos = {}, final JoystickPos = {}", encoderPos, originalJoyPos, joyPos);
+						Robot.liftSubsystem.moveElevatorDown(joyPos);
+						//   		System.out.println("Moving Lift Down");
+					} else {
+						Robot.liftSubsystem.moveAtManualSpeedGiven(0);
+					}
+				}
+			}
+		}
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		double joyPos = Robot.m_oi.getLiftJoystick();
+		if(Robot.liftSubsystem.isBottomLimitDepressed()) {
+			System.out.println("Bottom Switch just got pushed.");
+			Robot.liftSubsystem.resetEncoder();
+
+			return true;
+		}
+		/*	else if(Robot.liftSubsystem.isTopLimitDepressed()) {
     		System.out.println("Top Switch just got pushed.");
-    		
-    		return true;
-    	}
-    	
-    */	if(Robot.intakeSubsystem.isArmDown) {
-    		if(Robot.liftSubsystem.readEncoderInInches() > 20) {
-    			if(joyPos < 0) {//this was -0.2
-    				logger.info("Arm is down, lift is to high, joystick is moving, ending ManualLiftOperator");
-    				return true;
-    			}
-    		}
-    	}
-    
-    	if(Robot.intakeSubsystem.homeButtonIsPressed()) {
-    		logger.info("Arm is down, cancelling ManualLiftCommand");
-    		return true;
-    	}
-    	return false;
-    	
-    	
-    }
-    
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.liftSubsystem.killMotors();
-    	logger.info("Ending Manual Lift Command, encoder inches = {}", Robot.liftSubsystem.readEncoderInInches());
-    }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	Robot.liftSubsystem.killMotors();
-    	logger.info("Interrupting Manual Lift Command, encoder inches = {}", Robot.liftSubsystem.readEncoderInInches());
-    }
+    		return true;
+    	}
+
+		 */	if(Robot.intakeSubsystem.isArmDown) {
+			 if(Robot.liftSubsystem.readEncoderInInches() > 20) {
+				 if(joyPos < 0) {//this was -0.2
+					 logger.info("Arm is down, lift is to high, joystick is moving, ending ManualLiftOperator");
+					 return true;
+				 }
+			 }
+		 }
+
+		// if(Robot.intakeSubsystem.homeButtonIsPressed()) {
+		//	 logger.info("Arm is down, cancelling ManualLiftCommand");
+		//	 return true;
+		// }
+		return false;
+
+
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.liftSubsystem.killMotors();
+		logger.info("Ending Manual Lift Command, encoder inches = {}", Robot.liftSubsystem.readEncoderInInches());
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		Robot.liftSubsystem.killMotors();
+		logger.info("Interrupting Manual Lift Command, encoder inches = {}", Robot.liftSubsystem.readEncoderInInches());
+	}
 }
